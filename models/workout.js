166f4f3d0cd7de4649670d84e411dd05e7d7f262
Nathan Hall <Nashing1@gmail.com>
2020-08-23 20:20:44 -0500

    added workout model
    added workout model

---
+++ models/workout.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema (
+    {
+        day:{
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: 'please provide type of exercise'
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: 'please provide name of exercise'
+                },
+                duration: {
+                    type: Number,
+                    required: 'please provide duration of exercise'
+                },
+                weight:{
+                    type: Number
+                },
+                reps:{
+                    type: Number
+                },
+                sets:{
+                    type: Number
+                },
+                distance:{
+                    type: Number
+                }
+            }
+        ]
+    },
+    {
+        toJSON:{
+            virtuals: true
+        }
+    }
+);
+
+workoutSchema.virtual("totalDuration").get(function(){
+    return this.exercises.reduce((total, exercise) =>{
+        return total+exercise.duration;
+    },0);
+});
+
+const Workout = mongoose.model("Workout",workoutSchema)
+
+module.exports = Workout;
